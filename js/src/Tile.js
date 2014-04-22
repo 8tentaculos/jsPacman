f50a575d86f7aedf479877f489f6830e86d07cc2
Alberto Masuelli <alberto.masuelli@gmail.com>
2014-04-22 15:53:49 -0300

    initial

---
+++ js/src/Tile.js
@@ -0,0 +1,60 @@
+﻿define(['jquery'], function($) {
+
+    var Tile = function(code, col, row, map) {
+        this.code = code;
+        
+        this.col = col;
+        this.row = row;
+        
+        this.map = map;
+        
+        this.w = 16;
+        this.h = 16;
+        
+        if (this.isExit() || this.isHouse()) {
+            this.x = this.col * this.w +1; 
+        } else this.x = this.col * this.w + parseInt(this.w / 2);
+
+        this.y = this.row * this.h + parseInt(this.h / 2) + 1; // Original Pacman has tile's center at x : 4, y : 5 position.
+
+    };
+
+    $.extend(Tile.prototype, {
+        isWall : function() { return this.code === '='; },
+
+        isExit : function() { return this.code === 'e'; },
+
+        isHouse : function() { return this.code === 'h'; },
+
+        isOnlyLeft : function() { return this.code === '<'; },
+
+        isOnlyRight : function() { return this.code === '>'; },
+
+        get : function(dir) {
+            if (dir === 'u') return this.getU();
+            if (dir === 'd') return this.getD();
+            if (dir === 'l') return this.getL();
+            if (dir === 'r') return this.getR();
+            return null;
+        },
+        
+        getU : function() {
+            return this.map.getTile(this.col, this.row - 1) || null;
+        },
+        
+        getD : function() {
+            return this.map.getTile(this.col, this.row + 1) || null;
+        },
+        
+        getL : function() {
+            return this.map.getTile(this.col - 1, this.row) || null;
+        },
+        
+        getR : function() {
+            return this.map.getTile(this.col + 1, this.row) || null;
+        }
+    });
+    
+    return Tile;
+    
+});
---
+++ test/main.js
@@ -0,0 +1,22 @@
+﻿require.config({
+    baseUrl : '../js/src',
+    // paths config is relative to the baseUrl, and
+    // never includes a ".js" extension since
+    // the paths config could be for a directory.
+    paths: {
+        jquery : '../lib/jquery-1.10.2.min',
+        gameQuery : '../lib/jquery.gamequery-0.7.1',
+        chai : '../lib/chai',
+        maps : '../maps'
+    },
+    shim: {
+        gameQuery : ['jquery']
+    }
+});
+
+require([
+  '../../test/test'
+], function() {
+  if (window.mochaPhantomJS) { mochaPhantomJS.run(); }
+  else { mocha.run(); }
+});
