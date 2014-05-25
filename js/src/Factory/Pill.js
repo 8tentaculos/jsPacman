34e009a12a94292013c3573fa4995a8d1818c7bc
Alberto Masuelli <alberto.masuelli@gmail.com>
2014-05-24 23:04:34 -0300

    dot color

--- js/src/Factory/Pill.js
+++ js/src/Factory/Pill.js
@@ -7,14 +7,23 @@ define(['jquery', 'Item', 'gameQuery'], function($, Item) {
             return new Item($.extend({
                 w : 12,
                 h : 12,
+                aniBase : {
+                    imageURL : 'img/pills.png',
+                    numberOfFrame : 2,
+                    delta : 12, 
+                    rate : 450,
+                    type : $.gQ.ANIMATION_VERTICAL
+                },
+
+                defaultAnimation : 'white',
 
                 animations : {
-                    default : {
-                        imageURL : 'img/pills.png',
-                        numberOfFrame : 2,
-                        delta : 12, 
-                        rate : 450,
-                        type : $.gQ.ANIMATION_VERTICAL
+                    white : {},
+                    yellow : {
+                        offsetx : 12 + 4
+                    },
+                    red : {
+                        offsetx : (12 + 4) * 2
                     }
                 }
 
