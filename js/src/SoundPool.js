6e8d05db2eea69b9932432e07bc629aab1b7f06d
Alberto Masuelli <alberto.masuelli@gmail.com>
2014-12-08 23:12:16 -0300

    SoundPool

--- js/src/SoundPool.js
+++ js/src/SoundPool.js
@@ -2,21 +2,28 @@ define(['jquery', 'SoundWrapper'], function($, SoundWrapper) {
 
     var SoundPool = function(url, size) {
         this.pool = [];
+        this.url = url;
 
         for (var i = 0; i < size; i++) {
-            this._createSound(url);
+            this._createSound();
         }
 
     };
 
-    SoundPool.prototype._createSound = function(url) {
-        this.pool.push(new SoundWrapper(url));
+    SoundPool.prototype._createSound = function() {
+        this.pool.push(new SoundWrapper(this.url));
     };
 
     SoundPool.prototype.play = function() {
         var sw = this.pool.shift();
         sw.play();
-        this.pool.push(sw);
+        if (window.chrome) {
+            sw.audio = null;
+            this._createSound();
+        } else {
+            this.pool.push(sw);
+        }
+        
     };
 
     return SoundPool;
