801da89c8d7366faef37f624841f6fa035821e7a
Alberto Masuelli <alberto.masuelli@gmail.com>
2020-11-28 16:51:11 -0300

    Fix tests

--- src/js/factory/makeMsPacman.js
+++ src/js/factory/makeMsPacman.js
@@ -5,6 +5,6 @@ export const SPRITE_MS_PACMAN = 'SPRITE_MS_PACMAN';
 export default (options) => new Pacman({
     x : 452,
     y : 848,
-    type : SPRITE_MS_PACMAN
+    type : SPRITE_MS_PACMAN,
     ...options
 });
